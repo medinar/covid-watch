5b3625e38e60f1e45180b46c2c2ed2ee9df53188
medinar <rommel.d.medina@gmail.com>
2021-08-19 00:18:39 -0700

    Initial commit

---
+++ src/main/java/com/medinar/covid19app/Covid19App.java
@@ -0,0 +1,13 @@
+package com.medinar.covid19app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Covid19App.class, args);
+    }
+
+}
---
+++ src/test/java/com/medinar/covid19app/Covid19AppTests.java
@@ -0,0 +1,13 @@
+package com.medinar.covid19app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19AppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
