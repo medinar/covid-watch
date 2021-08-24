89b9eca8ef3491bd8f8b617f1ee34948dc28dd68
medinar <rommel.d.medina@gmail.com>
2021-08-23 22:07:05 -0700

    add error pages

--- src/main/java/com/medinar/covidwatch/controller/GlobalExceptionHandler.java
+++ src/main/java/com/medinar/covidwatch/controller/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.medinar.covidwatch.controller;
 import com.medinar.covidwatch.exception.ContinentalCasesNotFoundException;
 import com.medinar.covidwatch.exception.GlobalCasesNotFoundException;
 import com.medinar.covidwatch.exception.InternationalCasesNotFoundException;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,17 +16,50 @@ import org.springframework.web.servlet.ModelAndView;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final String MSG = "message";
+    
     @ExceptionHandler({
             GlobalCasesNotFoundException.class,
             ContinentalCasesNotFoundException.class,
-            InternationalCasesNotFoundException.class
+            InternationalCasesNotFoundException.class,
+            IOException.class
     })
     public ModelAndView handle(Exception ex) {
 
         var mv = new ModelAndView();
-        mv.addObject("message", ex.getMessage());
-        mv.setViewName("404");
+        mv.addObject(MSG, ex.getMessage());
+        mv.setViewName("error/404");
 
         return mv;
     }
+    
+    @ExceptionHandler(InterruptedException.class)
+    public ModelAndView handleInterruptedException(Exception ex) {
+
+        var mv = new ModelAndView();
+        mv.addObject(MSG, ex.getMessage());
+        mv.setViewName("error/500");
+
+        return mv;
+    }
+    
+    @ExceptionHandler(ExecutionException.class)
+    public ModelAndView handleExecutionException(Exception ex) {
+
+        var mv = new ModelAndView();
+        mv.addObject(MSG, ex.getMessage());
+        mv.setViewName("error/500");
+
+        return mv;
+    }
+    
+//    @ExceptionHandler(Exception.class)
+//    public ModelAndView handleException(Exception ex) {
+//
+//        var mv = new ModelAndView();
+//        mv.addObject(MSG, ex.getMessage());
+//        mv.setViewName("error/generic");
+//
+//        return mv;
+//    }
 }
